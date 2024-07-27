f4f8eccdcd45d8065bc7f2405e281432582225b5
Ali43555 <143989338+Ali43555@users.noreply.github.com>
2024-07-28 01:59:28 +0500

    updated services
    Updated the services section responsive

--- components/Services.js
+++ components/Services.js
@@ -18,78 +18,78 @@ const Services = () => {
                     Services I Offer.
                 </motion.p>
             </div>
-            
-            <div className='flex flex-wrap justify-evenly'>
 
-                    <motion.div 
-                      initial={{
-                          scale: 0,
-                          rotate: 0, 
-                        }}
-                      whileInView={{
-                          scale: 1,
-                          rotate: 0, 
-                        }} 
-                      transition = {{duration : 0.5 , delay : 0}}
-                      className='m-2 p-5 text-center justify-center items-center shadow-lg shadow-slate-300 rounded-lg '
-                    >
-                      <img 
-                        src="images/designing.webp"
-                        width={350}
-                        height={250}
-                        className='m-auto mt-10 '
-                      />
-                      <h3 className='py-5 text-2xl font-semibold text-slate-800 '>Beautiful Design</h3>
-                      <p className='text-slate-800 tracking-wide text-center w-96'>Creating elegant design suited for your needs following core design theory.</p>
-                    </motion.div>
+            <div className='flex flex-wrap justify-evenly'>    
+
+                        <motion.div 
+                          initial={{
+                              scale: 0,
+                              rotate: 0, 
+                            }}
+                          whileInView={{
+                              scale: 1,
+                              rotate: 0, 
+                            }} 
+                          transition = {{duration : 0.5 , delay : 0}}
+                          className='m-2 p-5 text-center justify-center items-center shadow-lg shadow-slate-300 rounded-lg '
+                        >
+                          <img 
+                            src="images/designing.webp"
+                            width={350}
+                            height={250}
+                            className='m-auto mt-10'
+                          />
+                          <h3 className='py-5 text-2xl font-semibold text-slate-800 '>Beautiful Design</h3>
+                          <p className='text-slate-800 tracking-wide text-center lg:w-96'>Creating elegant design suited for your needs following core design theory.</p>
+                        </motion.div>
 
 
-                    <motion.div 
-                      initial={{
-                          scale: 0,
-                          rotate: 0, 
-                        }}
-                      whileInView={{
-                          scale: 1,
-                          rotate: 0, 
-                        }} 
-                      transition = {{duration : 0.5 , delay : 0}}
-                      className='m-2 p-5 text-center justify-center items-center shadow-lg shadow-slate-300 rounded-lg '
-                    >
-                      <img 
-                        src="images/coding.webp"
-                        width={350}
-                        height={250}
-                        className='m-auto mt-10 '
-                      />
-                      <h3 className='py-5 text-2xl font-semibold text-slate-800 '>Code your Dream project</h3>
-                      <p className='text-slate-800 tracking-wide text-center w-96'>Translating complex requirements into intuitive, user-friendly applications using modern technologies.</p>
-                    </motion.div>
-                    
+                        <motion.div 
+                          initial={{
+                              scale: 0,
+                              rotate: 0, 
+                            }}
+                          whileInView={{
+                              scale: 1,
+                              rotate: 0, 
+                            }} 
+                          transition = {{duration : 0.5 , delay : 0}}
+                          className='m-2 p-5 text-center justify-center items-center shadow-lg shadow-slate-300 rounded-lg '
+                        >
+                          <img 
+                            src="images/coding.webp"
+                            width={350}
+                            height={250}
+                            className='m-auto mt-10 '
+                          />
+                          <h3 className='py-5 text-2xl font-semibold text-slate-800 '>Code your Dream project</h3>
+                          <p className='text-slate-800 tracking-wide text-center lg:w-96'>Translating complex requirements into intuitive, user-friendly applications using modern technologies.</p>
+                        </motion.div>
+                        
 
-                    <motion.div 
-                      initial={{
-                          scale: 0,
-                          rotate: 0, 
-                        }}
-                      whileInView={{
-                          scale: 1,
-                          rotate: 0, 
-                        }} 
-                      transition = {{duration : 0.5 , delay : 0}}
-                      className='m-2 p-5 text-center justify-center items-center shadow-lg shadow-slate-300 rounded-lg '
-                    >
-                      <img 
-                        src="images/consulting.webp"
-                        width={350}
-                        height={250}
-                        className='m-auto mt-10 '
-                      />
-                      <h3 className='py-5 text-2xl font-semibold text-slate-800 '>Consulting</h3>
-                      <p className='text-slate-800 tracking-wide text-center w-96'>Giving you strategic advice to boost innovation and efficiency.</p>
-                    </motion.div>
+                        <motion.div 
+                          initial={{
+                              scale: 0,
+                              rotate: 0, 
+                            }}
+                          whileInView={{
+                              scale: 1,
+                              rotate: 0, 
+                            }} 
+                          transition = {{duration : 0.5 , delay : 0}}
+                          className='m-2 p-5 text-center justify-center items-center shadow-lg shadow-slate-300 rounded-lg'
+                        >
+                          <img 
+                            src="images/consulting.webp"
+                            width={350}
+                            height={250}
+                            className='m-auto mt-10 '
+                          />
+                          <h3 className='py-5 text-2xl font-semibold text-slate-800 '>Consulting</h3>
+                          <p className='text-slate-800 tracking-wide text-center lg:w-96'>Giving you strategic advice to boost innovation and efficiency.</p>
+                        </motion.div>
+               
             </div>
-
         </div>  
     </section>
   )
